dd1eb23108ef8be6bf72a7de9028a12bd25769db
Deepak Kumar <dk1184366@gmail.com>
2026-05-21 05:10:50 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,7 @@ const chatRoutes = require("./routes/chatRoutes");
 const contactRoutes = require("./routes/contactRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(cors());
@@ -23,8 +22,8 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/digital-notice-board", {
   serverSelectionTimeoutMS: 2000
 })
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log("MongoDB Connection Error: ", err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("MongoDB Connection Error: ", err));
 
 // Routes
 app.use("/api/auth", authRoutes);
