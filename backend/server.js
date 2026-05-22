0a795064f9fef1293d972266573f9aff23f5cf51
Deepak Kumar <dk1184366@gmail.com>
2026-05-22 22:18:59 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const contactRoutes = require("./routes/contactRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
